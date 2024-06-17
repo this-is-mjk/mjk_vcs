e7553cf9f9d93dd335828f524df13896dc19574a
this-is-mjk <mjlivetowork@gmail.com>
2024-06-17 20:02:35 +0530

    working on the index file work

--- pkg/commands/add.go
+++ pkg/commands/add.go
@@ -1,5 +1,19 @@
 package commands
 
+import (
+	creator "github.com/this-is-mjk/mjk/pkg/utils/componentCreater"
+	"github.com/this-is-mjk/mjk/pkg/utils/fileUtils"
+)
+
 func Add() {
-	// yet to implement
+	pwd := fileUtils.Get_pwd()
+	creator.StageFile(pwd)
+	// fileUtils.WriteFile(pwd+"/.mjk/index", *bytes.NewBuffer([]byte(creator.StageFile(pwd))))
+	// files := strings.Split(string(fileUtils.ReadFile(pwd+"/.mjk/index")), "\n")
+	// for _, file := range files {
+	// 	if file == "" {
+	// 		continue
+	// 	}
+	// 	fmt.Println(file)
+	// }
 }
